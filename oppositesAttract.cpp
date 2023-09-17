b490f25163a9c1ac948aa07cbfdfa3bcd31045d5
dinaxhtml <108067899+dinaxhtml@users.noreply.github.com>
2023-09-17 21:30:38 +0300

    Add files via upload

---
+++ oppositesAttract.cpp
@@ -0,0 +1,25 @@
+/*
+codewars: Opposites Attract
+
+Timmy & Sarah think they are in love, but around where they live,
+they will only know once they pick a flower each.
+If one of the flowers has an even number of petals and the other has an odd number of petals
+it means they are in love.
+
+Write a function that will take the number of petals of each flower
+and return true if they are in love and false if they aren't.
+*/
+#include <iostream>
+using namespace std;
+
+bool loveFunc(int f1, int f2) {
+  return f1%2!=f2%2;
+}
+
+int main() {
+
+    cout<<loveFunc(5,4)<<endl;//1 true
+    cout<<loveFunc(5,1)<<endl;//0 falses
+
+    return 0;
+}
