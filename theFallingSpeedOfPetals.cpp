f5012695a63399c0e02f6950a5d939c280e8a02b
dinaxhtml <108067899+dinaxhtml@users.noreply.github.com>
2023-09-25 22:07:27 +0300

    Add files via upload

---
+++ theFallingSpeedOfPetals.cpp
@@ -0,0 +1,34 @@
+/*
+codewars: The falling speed of petals
+
+When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot.
+The petals start to fall in late April.
+
+Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s),
+and it takes 80 seconds for the petal to reach the ground from a certain branch.
+
+Write a function that receives the speed (in cm/s) of a petal as input,
+and returns the time it takes for that petal to reach the ground from the same branch.
+
+Notes:
+The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+Pay attention to the data types.
+If the initial velocity is non-positive, the return value should be 0
+*/
+
+#include <iostream>
+using namespace std;
+
+double SakuraFall(double v) {
+  if(v<=0)
+    return 0;
+return 400/v;
+}
+
+int main() {
+    cout<<SakuraFall(5)<<endl;//80
+    cout<<SakuraFall(10)<<endl;//40
+    cout<<SakuraFall(200)<<endl;//2
+
+    return 0;
+}
